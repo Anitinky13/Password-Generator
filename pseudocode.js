82652daaa52b5f4e5bda8150f2578d9909f7147b
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-14 14:20:40 -0500

    functions written

---
+++ pseudocode.js
@@ -0,0 +1,12 @@
+//generate button, add eventlistener     (done)
+//start function
+//window prompt for length
+//window confirm for lower case letter
+//window confirm for upper case letter
+//window confirm for numerics
+//window confirm for symbols
+//create arrays for each character set
+//function that compiles an array based on answers/ arrays methods
+//function that generates a password based on the arrays selected and of the selected length(use math.floor math.random)
+//get handle on password field
+//display generated password in the password field
--- script.js
+++ script.js
@@ -1,31 +1,80 @@
-window.alert("Welcome to Password Generator!");
-
-//Function to generate a random numeric value
-var randomNumber = function (min, max) {
-  var value = Math.floor(Math.random() * (max - min) + min);
-
-  return value;
-};
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var password = [
   {
     char: 0,
     lower: false,
     upper: false,
+    number: false,
     special: false,
   },
 ];
-
 //function criteria
 var chooseCriteria = function () {
   //first question: how many characters
   password.char = window.prompt(
     "How many characters would you like your password to have? Select a number between 8 and 128."
   );
+  console.log("char:" + password.char);
 
-  console.log("char;" + password.char);
+  //prompt valid: 8, 128
+
+  if (password.char >= 8 && password.char <= 128) {
+    //second question: lowercase?
+    password.lower = window.confirm(
+      "Would you like you to have a low case letter in your password? \nSelect OK for yes or CANCEL for no."
+    );
+
+    //Include Uppercase?
+    password.upper = window.confirm(
+      "Would you like to have a uppercase letter in your password? \nselect OK for yes or CANCEL for no."
+    );
+
+    //include numbers?
+    password.numbers = window.confirm(
+      "Would you like to have numbers in your password? \nselect OK for yes or CANCEL for no."
+    );
+
+    // include symbols?
+    password.symbol = window.confirm(
+      "would you like your password to contain symbols? \nselect OK for yes or CANCEL for no."
+    );
+
+    //choose at least one criteria
+    if (
+      password.lower == false &&
+      password.upper == false &&
+      password.number == false &&
+      password.symbol == false
+    ) {
+      //inform user critera is not valid and start prompts again
+      window.alert(
+        "You did not select enough criteria to create your password. Please try again."
+      );
+      return selectCriteri();
+    } else {
+      //display criteria selected and ask to confirm
+      window.alert(
+        "Password is ready to go. Please review the criteria you chose. If you are satisfied, click on the GENERATE PASSWORD button to create your new password. Otherwise, refresh the page and start over. n\nNumber of Characters: " +
+          password.char +
+          "\nLowercase Characters Included? " +
+          password.lower +
+          "\nUppercase Characters Included? " +
+          password.upper +
+          "\nNumber Characters Included? " +
+          password.number +
+          "\nSymbol Characters Included? " +
+          password.symbol
+      );
+    }
+  } else if (!password.char) {
+    window.alert(
+      "Welcome to Password Generator. Please answer the following questions to get you started."
+    );
+    return selectCriteria();
+  } else {
+    window.alert("Please enter a valid number. Thank you!");
+    return selectCriteria();
+  }
 };
 
 // Write password to the #password input
@@ -35,6 +84,11 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+//welcome message
+window.alert(
+  "Welcome to Password Generator! Please answer the following prompts to generate a password"
+);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
