7b7fdf714cc936ef0f341805c13546bb3a6946a3
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-14 00:51:01 -0500

    characters number

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+window.alert("Welcome to Password Generator!");
+
+//Function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min) + min);
+
+  return value;
+};
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = [
+  {
+    char: 0,
+    lower: false,
+    upper: false,
+    special: false,
+  },
+];
+
+//function criteria
+var chooseCriteria = function () {
+  //first question: how many characters
+  password.char = window.prompt(
+    "How many characters would you like your password to have? Select a number between 8 and 128."
+  );
+
+  console.log("char;" + password.char);
+};
 
 // Write password to the #password input
 function writePassword() {
